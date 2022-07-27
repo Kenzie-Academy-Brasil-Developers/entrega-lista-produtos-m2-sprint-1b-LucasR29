ad76e6138ee01134691a7223e6c530d92bd4d548
Lucas <lrigon2904@gmail.com>
2022-07-27 19:01:10 -0300

    finalização

--- src/js/dataProdutos.js
+++ src/js/dataProdutos.js
@@ -7,7 +7,7 @@ const produtos = [
         img: "./src/img/banana.png"
     },
     {
-        nome: 'Morango',
+        nome: 'Morango 5kg',
         preco: 2,
         secao: 'Hortifruti',
         categoria: 'fruta',
@@ -33,5 +33,27 @@ const produtos = [
         secao: 'Laticínio',
         categoria: 'Leite',
         img: './src/img/leite.png'
+    },
+    {
+        nome: 'Bolo',
+        preco: 10,
+        secao: 'Panificadora',
+        categoria: 'doce',
+        img: './src/img/bolo.png'
+    },
+    {
+        nome: 'Yorgut de Morango',
+        preco: 3,
+        secao: 'Laticínios',
+        categoria: 'doce',
+        img: './src/img/yorgut.png'
+    },
+    {
+        nome: 'Torta Maçã',
+        preco: 4,
+        secao: 'Panificadora',
+        categoria: 'doce',
+        img: './src/img/torta.png'
     }
 ]
+
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,84 @@
-// seu código aqui
+const currency = function(number){
+    return new Intl.NumberFormat('pt-BR', {style: 'currency',currency: 'BRL', minimumFractionDigits: 2}).format(number);
+};
+
+const ulProducts = document.getElementById('listaProdutos')
+const spanSoma = document.getElementById('somaValores')
+const buttonAll = document.getElementById('buttonAll')
+const buttonHortifruti = document.getElementById('buttonHortifruti')
+const buttonPanificadora = document.getElementById('buttonPanificadora')
+const buttonLaticinios = document.getElementById('buttonLaticinios')
+const searchBar = document.getElementById('searchBar')
+const searchButton = document.getElementById('searchButton')
+
+
+function createCards(section = 'none') {
+    let sum = 0
+    for(i in produtos){
+        if(produtos[i].secao == section || section == 'none'){
+            const keyWords = document.createElement('span')
+            keyWords.innerText = (produtos[i].nome + produtos[i].secao)
+            keyWords.style.display = 'none'
+
+            const liProducts = document.createElement('li')
+            ulProducts.appendChild(liProducts)
+        
+            const h3Products = document.createElement('h3')
+            h3Products.innerText = produtos[i].nome
+        
+            const imgProducts = document.createElement('img')
+            imgProducts.src = produtos[i].img
+           
+            const spanProducts = document.createElement('span')
+            spanProducts.innerText = produtos[i].secao
+            
+            const pProdutos = document.createElement('p')
+            pProdutos.innerText = currency(produtos[i].preco)
+        
+            liProducts.append(keyWords, imgProducts, h3Products, spanProducts, pProdutos)
+    
+            sum = sum + produtos[i].preco
+        }
+    }
+    spanSoma.innerText = ''
+    spanSoma.innerText = currency(sum)
+}
+
+createCards()
+
+buttonAll.addEventListener('click', function(){
+    ulProducts.innerHTML = ''
+    createCards()
+})
+
+buttonHortifruti.addEventListener('click', function() {
+    ulProducts.innerHTML = ''
+    createCards('Hortifruti')
+})
+
+buttonPanificadora.addEventListener('click', function() {
+    ulProducts.innerHTML = ''
+    createCards('Panificadora')
+})
+
+buttonLaticinios.addEventListener('click', function() {
+    ulProducts.innerHTML = ''
+    createCards('Laticínios')
+})
+
+const list = document.querySelector('.ulCards')
+
+searchBar.addEventListener('keyup', function(e){
+    let term = e.target.value.toLowerCase()
+    const products = list.getElementsByTagName('li')
+    
+    Array.from(products).forEach(function(product){
+        const name = product.firstElementChild.textContent
+        if(name.toLowerCase().indexOf(term) != -1){
+            product.style.display = 'block'
+        }else{
+            product.style.display = 'none'
+        }
+    })
+})
+
